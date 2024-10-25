98db4476335a306337ef03391d20fe7bf063012f
binnuw <133031300+binnuw@users.noreply.github.com>
2024-10-25 23:09:40 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 13.89 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -2,7 +2,7 @@ class Solution {
     public long solution(long n) {
         long answer = 0;
         for(long i = 1; i <= n/2 +1; i++) {
-            if(n / i == i && n % i == 0) {
+            if(i * i == n) {
                 answer = (i + 1)*(i + 1);
                 break;
             } else {
